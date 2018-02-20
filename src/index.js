3226d89bfc35d6472bf9fd32f5b09eb2ba67522f
Mufaddal Motorwala <mufaddal.motorwala@rbbideas.com>
2018-02-20 18:19:20 +0400

    First commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyCR9zTn-b40n7r-y3nzx58ee2HZ4uwv2mE';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+YTSearch({ key: API_KEY, term: 'sea'}, function(data){
+  console.log(data);
+});
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
+  
+  <SearchBar />
+  
   , document.querySelector('.container'));
